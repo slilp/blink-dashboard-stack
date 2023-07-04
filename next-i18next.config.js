52118c348c94a5144f18cdb311e09ef28557f780
slil_puangpoom <slil_puangpoom@gmail.com>
2023-07-04 20:43:34 +0700

    fix : i18n not working

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-i18next').UserConfig} */
 // **** need to rerun when changing locales files
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
